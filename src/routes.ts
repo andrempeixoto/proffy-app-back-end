29b23f109ff717db4d74acb8d484cf65beba075c
Andre Peixoto <peixoto.amp@gmail.com>
2021-04-11 00:39:02 -0300

    Adds fn convertHourToMinutes for classSchedule

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,43 @@
 import express from 'express';
+import db from './database/connection';
 
 const routes = express.Router(); 
 
-routes.get('/', (request, response) => {
-  return response.json({
-    message: "it worked"
+routes.post('/classes', async (request, response) => {
+  
+  const {
+    name,
+    avatar,
+    telegram,
+    bio,
+    subject,
+    price,
+    schedule
+  } = request.body;
+  
+  const insertedUsersIds = await db('users').insert({
+    name,
+    avatar,
+    telegram,
+    bio,
   })
+
+  const user_id = insertedUsersIds[0];
+
+  const insertedClassesIds await db('classes').insert({
+    subject,
+    price,
+    user_id,
+  })
+
+  const classes_id = insertedClassesIds[0];
+  
+  await db('class_schedule').insert({
+    schedule,
+    classes_id,
+  })
+
+  return response.send();
 });
 
 export default routes;
